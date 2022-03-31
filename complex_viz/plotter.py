6a7f777467f40bb71ad8d25d43e2c5b92c49f945
Auberon López <auberon@adadev.org>
2022-03-31 12:56:50 -0700

    Allowed for tracking ticks separately from inputs

--- complex_viz/plotter.py
+++ complex_viz/plotter.py
@@ -2,12 +2,14 @@ from .tracker import track_growth
 
 from matplotlib import pyplot as plt
 
-def plot_growth(f, ns):
+def plot_growth(f, ns, ticks=None):
     x, y = track_growth(f, ns)
+    if ticks is not None:
+        x = ticks
     plt.plot(x, y, label=f.__name__)
 
 
-def compare_growth(fs, ns):
+def compare_growth(fs, ns, ticks=None):
     for f in fs:
-        plot_growth(f, ns)
+        plot_growth(f, ns, ticks=ticks)
     plt.legend(loc="upper left")
